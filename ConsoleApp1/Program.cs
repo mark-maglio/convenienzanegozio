800031bccf6c2523a058b33961c8b76a47a02359
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-21 08:35:24 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Negozio;
+using Negozio12;
+
+namespace Convenienza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il prezzo totale: ");
+            double totale = double.Parse(Console.ReadLine());
+            double n = ScontoNegozio.Negozio(totale);
+            double n12 = ScontoNegozio12.Negozio12(totale);
+            if (n < n12)
+            {
+                Console.WriteLine("E' più conveniente il primo negozio");
+            }
+            else if (n > n12)
+            {
+                Console.WriteLine("E' più conveniente il primo negozio");
+            }
+            else
+            {
+                Console.WriteLine("Sono ugualmente convenienti");
+            }
+            Console.ReadLine();
+        }
+    }
+}
